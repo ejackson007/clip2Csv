cd6f0d1d39c812e306e59664183ffae01241feae
Evan Jackson <evan@Evans-MacBook-Air.local>
2019-11-03 20:07:28 -0600

    formatting

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
-import pyperclip
+import clipboard
 
-pyperclip.paste()
+#take the contents of clipboard into text
+copied = clipboard.paste()
+#get rid of excess white space to make transfering to list easier
+copied.lstrip()
+#toList = list(copied.split(" "))
+
+clipboard.copy(copied)
+
+#print(copied)
